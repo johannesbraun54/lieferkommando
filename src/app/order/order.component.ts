950c6a5a726b5c9d98348b12e3c55c9350ed8a8e
johannesbraun54 <johannesbraun02@gmail.com>
2024-02-24 15:54:45 +0100

    order scss

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,22 +1,29 @@
 import { Component, inject } from '@angular/core';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { User } from '../models/user.class';
-import { FormsModule } from '@angular/forms';
-import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { FormBuilder, FormsModule, NgForm, Validators } from '@angular/forms';
+import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { NgIf } from '@angular/common';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { Meal } from '../models/meal.class';
 import { ShoppingBasketService } from '../service/shopping-basket.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-order',
   standalone: true,
+  providers: [provideNativeDateAdapter()],
   imports: [MatDialogModule, 
     MatButtonModule,
     MatInputModule,
@@ -25,7 +32,11 @@ import { ShoppingBasketService } from '../service/shopping-basket.service';
     MatProgressBarModule,
     NgIf,
     MatDatepickerModule,
-    MatNativeDateModule],
+    MatNativeDateModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatCardModule,
+    ReactiveFormsModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
@@ -36,9 +47,32 @@ user = new User();
 meal = new Meal();
 firestore: Firestore = inject(Firestore);
 purchaseInJson!:any;
+step = 0;
+
+
 
 constructor(public shoppingBasketService : ShoppingBasketService){
 this.shoppingBasketService.getPurchase();
+}
+
+onSubmit(nameForm: NgForm){
+  if(nameForm.valid){
+    console.log('nameform is valid');
+  }else{
+    console.log('nameform is not valid');
+  }
+}
+
+setStep(index: number) {
+  this.step = index;
+}
+
+nextStep() {
+  this.step++;
+}
+
+prevStep() {
+  this.step--;
 }
 
 getActuallyTime(){
